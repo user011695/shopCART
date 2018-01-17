810b7f578c074ea3216775c50a4e8c09bc281e35
Shivam Kumar verma <shivam1695@gmail.com>
2018-01-17 12:08:47 +0530

    Update webapp settings __init__ file

--- webapp/settings/__init__.py
+++ webapp/settings/__init__.py
@@ -2,11 +2,11 @@ from .base import *
 
 from .production import *
 
-# try:
-#     from .local import *
-# except:
-#     pass
-#
+try:
+    from .local import *
+except:
+    pass
+
 # try:
 #     from .local_justin import *
 # except:
